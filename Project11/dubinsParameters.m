f83a4b0e72aa014be92853e636abefa8660078ff
cgrooves <caleb.j.groves@gmail.com>
2018-03-30 20:14:05 -0600

    Filled out algorithm 7 (untested)

--- Project11/dubinsParameters.m
+++ Project11/dubinsParameters.m
@@ -28,92 +28,105 @@
 function dubinspath = dubinsParameters(start_node, end_node, R)
 
   ell = norm(start_node(1:2)-end_node(1:2));
-  if ell<2*R,
+  if ell<2*R
       disp('The distance between nodes must be larger than 2R.');
       dubinspath = [];
   else
     
-    ps   = ;
-    chis = ;
-    pe   = ;
-    chie = ;
-    
+    ps   = [start_node(1); start_node(2); start_node(3)];
+    chis = start_node(4);
+    pe   = [end_node(1); end_node(2); end_node(3)];
+    chie = end_node(4);
 
-    crs = ;
-    cls = ;
-    cre = ;
-    cle = ;
+    crs = ps + R*rotz(pi/2)*[cos(chis), sin(chis), 0]';
+    cls = ps + R*rotz(-pi/2)*[cos(chis), sin(chis), 0]';
+    cre = pe + R*rotz(pi/2)*[cos(chie), sin(chie), 0]';
+    cle = pe + R*rotz(-pi/2)*[cos(chie), sin(chie), 0]';
     
-   
     % compute L1
-    theta = ;
-    L1 = ;
+    a = cre - crs;
+    theta = acos(a(1)/norm(a));
+    L1 = norm(crs - cre) + R*mod(2*pi + mod(theta - pi/2,2*pi) - ...
+        mod(chis - pi/2,2*pi),2*pi) + R*mod(2*pi + mod(chie - pi/2,2*pi)...
+        - mod(theta - pi/2, 2*pi), 2*pi);
+    
     % compute L2
-    ell = ;
-    theta = ;
-    theta2 = ;
-    if isreal(theta2)==0, 
+    ell = norm(cle - crs);
+    a = cle - crs;
+    theta = acos(a(1)/norm(a));
+    theta2 = theta - pi/2 + asin(2*R/ell);
+    if isreal(theta2)==0 
       L2 = 9999; 
     else
-      L2 = ;
+      L2 = sqrt(ell^2 - 4*R^2) + R*mod(2*pi + mod(theta2, 2*pi) - mod(...
+          chis - pi/2, 2*pi), 2*pi) + R*mod(2*pi + mod(theta2 + pi, 2*pi)...
+          - mod(chie + pi/2, 2*pi), 2*pi);
     end
     % compute L3
-    ell = ;
-    theta = ;
-    theta2 = ;
-    if isreal(theta2)==0,
+    ell = norm(cre - cls);
+    a = cre - cls;
+    theta = acos(a(1)/norm(a));
+    theta2 = acos(2*R/ell);
+    if isreal(theta2)==0
       L3 = 9999;
     else
-      L3 = ;
+      L3 = sqrt(l^2 - 4*R^2) + R*mod(2*pi + mod(chis + pi/2, 2*pi) - ...
+          mod(theta + theta2, 2*pi), 2*pi) + R*mod(2*pi + mod(chie - ...
+          pi/2, 2*pi) - mod(theta + theta2 - pi, 2*pi), 2*pi);
     end
     % compute L4
-    theta = ;
-    L4 = ;
+    a = cls - cle;
+    theta = acos(a(1)/norm(a));
+    L4 = norm(cls - cle) + R*mod(2*pi + mod(chis + pi/2, 2*pi) - ...
+        mod(theta + pi/2, 2*pi), 2*pi) + R*mod(2*pi + mod(theta + pi/2, ...
+        2*pi) - mod(chie + pi/2, 2*pi), 2*pi);
     % L is the minimum distance
     [L,idx] = min([L1,L2,L3,L4]);
     e1 = [1; 0; 0];
-    switch(idx),
-        case 1,
-            cs = ;
-            lams = ;
-            ce = ;
-            lame = ;
-            q1 = ;
-            w1 = ;
-            w2 = ;
-        case 2,   
-            cs = ;
-            lams = ;
-            ce = ;
-            lame = ;
-            ell = ;
-            theta = ;
-            theta2 = ;
-            q1 = ;
-            w1 = ;
-            w2 = ;
-        case 3,
-            cs =;
-            lams =;
-            ce = ;
-            lame = ;
-            ell = ;
-            theta = ;
-            theta2 = ;
-            q1 = ;
-            w1 = ;
-            w2 = ;
-         case 4,
-            cs = ;
-            lams = ;
-            ce = ;
-            lame = ;
-            q1 = ;
-            w1 = ;
-            w2 = ;
+    switch(idx)
+        case 1
+            cs = crs;
+            lams = 1;
+            ce = cre;
+            lame = 1;
+            q1 = (ce - cs)/norm(ce - cs);
+            w1 = cs + R*rotz(-pi/2)*q1;
+            w2 = ce + R*rotz(-pi/2)*q1;
+        case 2 
+            cs = cls;
+            lams = -1;
+            ce = cre;
+            lame = 1;
+            ell = norm(ce - cs);
+            a = ce - cs;
+            theta = acos(a(1)/norm(a));
+            theta2 = theta - pi/2 + asin(2*R/ell);
+            q1 = rotz(theta2 + pi/2)*e1;
+            w1 = cs + R*rotz(theta2)*e1;
+            w2 = ce + R*rotz(theta2)*e1;
+        case 3
+            cs = cls;
+            lams = -1;
+            ce = cre;
+            lame = 1;
+            ell = norm(ce - cs);
+            a = ce - cs;
+            theta = acos(a(1)/norm(a));
+            theta2 = acos(2*R/ell);
+            q1 = rotz(theta + theta2 - pi/2)*e1;
+            w1 = cs + R*rotz(theta + theta2)*e1;
+            w2 = ce + R*rotz(theta + theta2 - pi/2)*e1;
+         case 4
+            cs = cls;
+            lams = -1;
+            ce = cle;
+            lame = -1;
+            q1 = (ce - cs)/norm(ce - cs);
+            w1 = cs + R*rotz(pi/2)*q1;
+            w2 = ce + R*rotz(pi/2)*q1;
     end
-    w3 = ;
-    q3 = ;
+    w3 = pe;
+    q3 = rotz(chie)*e1;
     
     % assign path variables
     dubinspath.ps   = ps;
